c871e00788dee391860daf337117e7348ac774e9
Ani <ani.stoicheva@gmail.com>
2015-05-26 18:46:13 +0300

    Adding some changes in views and urls.py. They need to be fixed.

--- What2Eat/website/urls.py
+++ What2Eat/website/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
+# from django.conf.url import patterns
+
+from . import views
 
 
-urlpatterns = patterns('website.views',
-    url(r'^$', 'index'),
-)
+urlpatterns = [
+    url(r"^$", views.index, name="index"),
+    url(r"info/$".views.info, name="info"),
+    url(r"^register/$", views.register, name="register"),
+    url(r"^logout/$", views.chat_logout, name="logout"),
+]
