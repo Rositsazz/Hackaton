bf9ef4f74c3d15c4f085b3880ec626d33232ed37
Ani <ani.stoicheva@gmail.com>
2015-05-27 12:47:30 +0300

    Fixing DB again.

--- What2Eat/website/models.py
+++ What2Eat/website/models.py
@@ -2,65 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    products = models.CharField(max_length=1000)
-    prepare_time = models.CharField(max_length=15)
-    instructions = models.CharField(max_length=5000)
-    image = models.CharField(max_length=300)
-    total_fat = models.FloatField()
-    protein = models.FloatField()
-    servings = models.PositiveSmallIntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class ProductToBuy(models.Model):
-    name = models.CharField(max_length=70)
-    amount = models.FloatField()
-    product_brand = models.CharField(blank=True, max_length=100)
-    shop_id = models.ForeignKey('Shop')
-    user_id = models.ForeignKey('User')
-
-    def __str__(self):
-        return self.name
-
-
-class Shop(models.Model):
-    shop_name = models.CharField(max_length=50)
-    card_barcode_length = models.IntegerField()
-
-    def __str__(self):
-        return self.shop_name
-
-
-class UserCard(models.Model):
-    user_id = models.ForeignKey('User')
-    shop_id = models.ForeignKey('Shop')
-    card_barcode = models.IntegerField()
-
-    def __str__(self):
-        return "{} - {}".format(self.user_id.username, self.shop_id.shop_name)
-
-
-class LastCookedRecipe(models.Model):
-    user_id = models.ForeignKey('User')
-    recipe_id = models.ForeignKey('Recipe')
-    date = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return self.recipe_id.name
-
-
-class FavouriteRepice(models.Model):
-    user_id = models.ForeignKey('User')
-    recipe_id = models.ForeignKey('Recipe')
-
-    def __str__(self):
-        return self.recipe_id.name
-
-
 class User(models.Model):
     GENDER_TYPES = (
         ('M', 'Male'),
@@ -80,3 +21,66 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    products = models.CharField(max_length=1000)
+    prepare_time = models.CharField(max_length=15)
+    instructions = models.CharField(max_length=5000)
+    image = models.CharField(max_length=300)
+    total_fat = models.FloatField()
+    protein = models.FloatField()
+    servings = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class LastCookedRecipe(models.Model):
+    user = models.ForeignKey('User')
+    recipe = models.ForeignKey('Recipe')
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.recipe_id.name
+
+
+class FavouriteRepice(models.Model):
+    user = models.ForeignKey('User')
+    recipe = models.ForeignKey('Recipe')
+
+    def __str__(self):
+        return self.recipe.name
+
+
+class ProductToBuy(models.Model):
+    name = models.CharField(max_length=70)
+    amount = models.FloatField()
+    product_brand = models.CharField(blank=True, max_length=100)
+    shop = models.ForeignKey('Shop')
+    user = models.ForeignKey('User')
+
+    def __str__(self):
+        return self.name
+
+
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=50)
+    card_barcode_length = models.IntegerField()
+
+    def __str__(self):
+        return self.shop_name
+
+
+class UserCard(models.Model):
+    user = models.ForeignKey('User')
+    shop = models.ForeignKey('Shop')
+    card_barcode = models.IntegerField()
+
+    def __str__(self):
+        return "{} - {}".format(self.user.username, self.shop.shop_name)
