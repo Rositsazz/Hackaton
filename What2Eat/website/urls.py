41671f9f52fddf0705dc7670c1de24f9687caa39
Rositsazz <ross_zz@mail.bg>
2015-05-27 12:13:12 +0300

    change html

--- What2Eat/website/urls.py
+++ What2Eat/website/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, patterns
+# from django.conf.url import patterns
 
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'index'),
+    # url(r'info/$', 'info'),
+    # url(r'register/$', 'register'),
+    # url(r'logout/$', 'logout'),
 )
