73d088e7fb131c64916e648acae6dc07a2991f08
Ani <ani.stoicheva@gmail.com>
2015-05-27 23:49:49 +0300

    Fixing a conflict in migrations.

---
+++ What2Eat/website/migrations/0011_merge.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0010_auto_20150527_1114'),
+    ]
+
+    operations = [
+    ]
