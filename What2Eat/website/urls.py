c75fa6d05c115c41fc2188a2068de3b926d29c5e
Rositsazz <ross_zz@mail.bg>
2015-05-27 12:55:55 +0300

    add function in view

--- What2Eat/website/urls.py
+++ What2Eat/website/urls.py
@@ -1,14 +1,6 @@
-
-from django.conf.urls import url
-
-# from django.conf.url import patterns
-
-from . import views
+from django.conf.urls import url, patterns
 
 
-urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    # url(r"info/$".views.info, name="info"),
-    # url(r"^register/$", views.register, name="register"),
-    # url(r"^logout/$", views.chat_logout, name="logout"),
-]
+urlpatterns = patterns('website.views',
+    url(r'^$', 'index'),
+)
