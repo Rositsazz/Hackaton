63c5a1a402fd8edde5be04da6356345c48facbff
Vladimir Petkov <vlado@itgbg.com>
2015-05-27 13:41:08 +0300

    fixing preapare_time type in filler.py

--- What2Eat/filler.py
+++ What2Eat/filler.py
@@ -11,10 +11,10 @@ with open("recipes_popular.json", "r") as f:
         if "mins" in recipe["prepare_time"]:
             splited = recipe["prepare_time"].split("mins")
             recipe["prepare_time"] = int(splited[0])
-        if "hrs"in recipe["prepare_time"]:
+        elif "hrs"in recipe["prepare_time"]:
             splited = recipe["prepare_time"].split("hrs")
             recipe["prepare_time"] = int(splited[0])*60
-        if "hr" in recipe["prepare_time"]:
+        elif "hr" in recipe["prepare_time"]:
             recipe["prepare_time"] = 60
         query = "INSERT INTO website_recipe (instructions, name, servings, products, image, total_fat, prepare_time, protein) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
         cursor.execute(query, (recipe["instructions"], recipe["name"], recipe["servings"], recipe["products"], recipe["image"], recipe["total_fat"], recipe["prepare_time"], recipe["protein"]))
