5af45a4b94da67def72c961a645331e5c7655bce
Vladimir Petkov <vlado@itgbg.com>
2015-05-28 21:38:18 +0300

    fixed duplication of recipes

--- What2Eat/filler.py
+++ What2Eat/filler.py
@@ -7,23 +7,29 @@ cursor = conn.cursor()
 with open("recipes_popular.json", "r") as f:
     contents = f.read()
     recipes = json.loads(contents)
+    recipes_to_now = []
 
     for recipe in recipes:
-        if "mins" in recipe["prepare_time"]:
-            splited = recipe["prepare_time"].split("mins")
-            recipe["prepare_time"] = int(splited[0])
-        elif "hrs"in recipe["prepare_time"]:
-            splited = recipe["prepare_time"].split("hrs")
-            recipe["prepare_time"] = int(splited[0])*60
-        elif "hr" in recipe["prepare_time"]:
-            recipe["prepare_time"] = 60
+        if recipe["name"] not in recipes_to_now:
+            recipes_to_now.append(recipe["name"])
 
-        coef = recipe["total_fat"] / recipe["protein"] if recipe["protein"] != 0 else 0.00005
-        coef = round(coef, 2)
+            if "mins" in recipe["prepare_time"]:
+                splited = recipe["prepare_time"].split("mins")
+                recipe["prepare_time"] = int(splited[0])
 
-        keywords = ""
+            elif "hrs"in recipe["prepare_time"]:
+                splited = recipe["prepare_time"].split("hrs")
+                recipe["prepare_time"] = int(splited[0])*60
 
-        query = "INSERT INTO website_recipe (instructions, name, servings, products, image, total_fat, prepare_time, protein, healthy_coef, keywords) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        cursor.execute(query, (recipe["instructions"], recipe["name"], recipe["servings"], recipe["products"], recipe["image"], recipe["total_fat"], recipe["prepare_time"], recipe["protein"], coef, keywords))
+            elif "hr" in recipe["prepare_time"]:
+                recipe["prepare_time"] = 60
+
+            coef = recipe["total_fat"] / recipe["protein"] if recipe["protein"] != 0 else 0.00005
+            coef = round(coef, 2)
+
+            keywords = ""
+
+            query = "INSERT INTO website_recipe (instructions, name, servings, products, image, total_fat, prepare_time, protein, healthy_coef, keywords) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(query, (recipe["instructions"], recipe["name"], recipe["servings"], recipe["products"], recipe["image"], recipe["total_fat"], recipe["prepare_time"], recipe["protein"], coef, keywords))
 
     conn.commit()
